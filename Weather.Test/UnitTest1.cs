d82531f79c5e232788e56c549a668180b675b675
Rob Gilliam <rob_gilliam@hotmail.com>
2022-08-18 17:53:56 +0100

    "Template all the things"
    Created webapi and test projects, and a solution (plus .gitignore) using
    'dotnet new'
    
    Also created .vscode support folder

---
+++ Weather.Test/UnitTest1.cs
@@ -0,0 +1,12 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Weather.Test;
+
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+}
